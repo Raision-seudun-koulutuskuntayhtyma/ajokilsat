00e182c3eee9f0a5f6cf92161087eab1712d2736
Tuomas Suutari <tuomas@nepnep.net>
2023-09-18 14:03:47 +0300

    Paremmat esimerkkimatkat

--- utils/store.ts
+++ utils/store.ts
@@ -35,164 +35,22 @@ let trips: Trip[] = [
     {
         id: newId(),
         vehicleId: 'car1',
-        description: 'Käynti Devin Testisen luona',
+        description: 'Työmatka',
+        timestampAtBegin: new Date(2023, 8, 18, 8, 15),
+        timestampAtEnd: new Date(2023, 8, 18, 9, 15),
+        routeDescription: 'Salo - Helsinki',
+        odometerAtBegin: 125052,
+        odometerAtEnd: 125118,
     },
     {
         id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Dan Testisen luona',
-    },
-];
+        vehicleId: 'car2',
+        description: 'Visiitti Ahvenanmaalle',
+        timestampAtBegin: new Date(2023, 8, 15, 10, 45),
+        timestampAtEnd: new Date(2023, 8, 15, 22, 35),
+        routeDescription: 'Tampere - Turku - Laiva - Ahvenanmaa',
+        odometerAtBegin: 125130,
+        odometerAtEnd: 125445,
 
-let extraTrips = [
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Dominic Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jackson Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti James Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Joel Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti John Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jillian Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jimmy Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Julie Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Kalle Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Laura Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Matti Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Noora Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Olli Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Petra Testisen luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Devin Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Dan Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Dominic Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jackson Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti James Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Joel Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti John Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jillian Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Jimmy Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Julie Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Kalle Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Laura Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Matti Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Noora Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Olli Kokeilijan luona',
-    },
-    {
-        id: newId(),
-        vehicleId: 'car1',
-        description: 'Käynti Petra Kokeilijan luona',
     },
 ];
